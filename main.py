be82c31d90ea10f0888259ce77beb337eb2b0122
Apel Mahmud <44221242+apl-mhd@users.noreply.github.com>
2022-08-01 20:15:33 +0600

    status code added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from signal import raise_signal
 from turtle import title
-from fastapi import Depends, FastAPI
+from urllib import response
+from fastapi import Depends, FastAPI, status, Response, HTTPException
 from pydantic import BaseModel
 from  blog.database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -17,10 +19,24 @@ def get_db():
     finally:
         db.close()
 
-@app.post('/blog')
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db:Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+@app.get('/blog')
+def all(db:Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+@app.get('/blog/{id}', status_code=200)
+def show(id, db:Session=Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"Blog with id {id} is not available")
+        #response.status_code = status.HTTP_404_NOT_FOUND
+        #return {'detail': f"Blog with id {id} is not available"}
+    return blog
